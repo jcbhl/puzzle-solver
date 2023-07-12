3f157aa642de0803ddf5c919f4a68d1fc73a9c28
Jacob <jlh2hb@virginia.edu>
2023-07-12 11:20:53 -0400

    Starting to write move finder and this sucks. Should refactor to array of offsets?

--- src/defs.rs
+++ src/defs.rs
@@ -1,14 +1,16 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub struct Point {
     pub x: usize,
     pub y: usize,
     pub z: usize,
 }
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub struct Position {
     pub center: Point,
     pub orientation: Orientation,
 }
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum Orientation {
     // viewed from the top
     FlatUp,              // ┴
--- src/main.rs
+++ src/main.rs
@@ -20,17 +20,17 @@ mod defs;
 use crate::defs::*;
 #[allow(dead_code)]
 #[derive(Debug)]
-enum State{
+enum State {
     PlacingFlat,
     PlacingFaceup,
     PlacingFacedown,
-    PlacingUpright
+    PlacingUpright,
 }
 struct Board {
     occupied: Array3<bool>,
     current_unfilled_layer: usize,
     cursor: Point,
-    state: State
+    state: State,
 }
 
 impl Board {
@@ -38,18 +38,18 @@ impl Board {
         Self {
             occupied: Array3::default((BOARD_SIZE, BOARD_SIZE, BOARD_SIZE)),
             current_unfilled_layer: 0,
-            cursor: Point{x:0, y:0, z:0},
-            state: PlacingFlat
+            cursor: Point { x: 0, y: 0, z: 0 },
+            state: State::PlacingFlat,
         }
     }
-    fn at(&self, point: Point) -> bool{
+    fn at(&self, point: Point) -> bool {
         return self.occupied[[point.x, point.y, point.z]];
     }
 }
 
 fn main() {
     let mut board = Board::new();
-    solve(&mut board);
+    // solve(&mut board);
 }
 
 fn solve(board: &mut Board) {
@@ -57,10 +57,15 @@ fn solve(board: &mut Board) {
 }
 
 fn get_next_placement(board: &mut Board) -> Option<Position> {
-    let ending_
-    while board.cursor != Point{x: BOARD_SIZE - 1, y:BOARD_SIZE - 1, z:board.current_unfilled_layer}) {
+    let ending_point = Point {
+        x: BOARD_SIZE - 1,
+        y: BOARD_SIZE - 1,
+        z: board.current_unfilled_layer,
+    };
 
-        increment_cursor_in_slice(&mut board.cursor)
+    while board.cursor != ending_point {
+        // if let try_orient
+        // increment_cursor_in_slice(&mut board.cursor)
     }
 
     println!(
@@ -68,15 +73,65 @@ fn get_next_placement(board: &mut Board) -> Option<Position> {
         board.current_unfilled_layer
     );
     println!("{:?}", board.occupied);
-
     None
 }
 
+fn inbounds_and_clear(board: &Board, point: &Point) -> bool {
+    return point.x <= BOARD_SIZE - 1 && point.y <= BOARD_SIZE - 1 && point.z <= BOARD_SIZE - 1;
+}
+fn try_orientations(board: &Board, point: &Point, state: State) -> Option<Position> {
+    match state {
+        State::PlacingFlat => {
+            // FlatUp
+            if inbounds_and_clear(
+                &board,
+                &Point {
+                    x: point.x,
+                    y: point.y,
+                    z: point.z,
+                },
+            ) && inbounds_and_clear(
+                &board,
+                &Point {
+                    x: point.x.wrapping_sub(1),
+                    y: point.y,
+                    z: point.z,
+                },
+            ) && inbounds_and_clear(
+                &board,
+                &Point {
+                    x: point.x + 1,
+                    y: point.y,
+                    z: point.z,
+                },
+            ) && inbounds_and_clear(
+                &board,
+                &Point {
+                    x: point.x,
+                    y: point.y.wrapping_sub(1),
+                    z: point.z,
+                },
+            ) {
+                return Some(Position {
+                    center: point.clone(),
+                    orientation: Orientation::FlatUp,
+                });
+            }
+            // FlatLeft
+            // FlatDown
+            // FlatRight
+            None
+        }
+        State::PlacingFaceup => todo!(),
+        State::PlacingFacedown => todo!(),
+        State::PlacingUpright => todo!(),
+    }
+}
 
-fn increment_cursor_in_slice(cursor: &mut (usize, usize)) {
-    cursor.0 += 1;
-    if cursor.0 == BOARD_SIZE {
-        cursor.0 = 0;
-        cursor.1 += 1;
+fn increment_cursor_in_slice(cursor: &mut Point) {
+    cursor.x += 1;
+    if cursor.x == BOARD_SIZE {
+        cursor.x = 0;
+        cursor.y += 1;
     }
 }
